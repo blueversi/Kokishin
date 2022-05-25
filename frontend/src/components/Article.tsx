5fd7770172b46b762055882d8b948fbcf42181e0
Rafał <riderbrand@gmail.com>
2022-05-25 17:48:00 +0200

    Add navlinks in article component

--- frontend/src/components/Article.tsx
+++ frontend/src/components/Article.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 interface ArticleProps {
   id: string;
@@ -15,37 +16,44 @@ const Article = (props: ArticleProps) => {
   return (
     <div className='main-list-item'>
       <div className='main-list-item-vote-bar'>
-        <a href='#'>
+        {/* TODO: Zamiast navlinka button z handlerem  ale to na etapie jak juz bd mongo śmigać z danymi */}
+        <NavLink to='/'>
           <i className='far fa-lightbulb'></i>
-        </a>
+        </NavLink>
       </div>
       <div className='main-list-item-content'>
         <h3>{props.tiitle}</h3>
         <span className='main-list-item-author'>
-          Dodane przez: <a href='#'>{props.author}</a>Dnia: {props.date}
+          Dodane przez: <NavLink to='/user/Blueversi'>{props.author} </NavLink>
+          Dnia: {props.date}
         </span>
-        <span className='tags'> do {props.category}</span>
+        <span className='tags'>
+          {' '}
+          do
+          <NavLink to={`./categories/${props.category}`}>
+            {' '}
+            {props.category}
+          </NavLink>
+        </span>
         <p>{props.description}</p>
       </div>
       <div className='main-list-item-photo'>
         <img src={props.img} alt='obrazek do posta' />
       </div>
       <div className='main-list-item-footer'>
-        <a href='#'>
+        <NavLink to={`./articles/data/nazwa#comments`}>
           <i className='far fa-comments'></i> 365
-        </a>
-        -
-        <a href='#'>
+        </NavLink>
+        {/* Dla poniższych w przyszłości handlery zamiast navlinkó na etapie mongo z danymi */}
+        <NavLink to={`./articles/data/nazwa#share`}>
           <i className='fas fa-share-alt'></i> Udostępnij
-        </a>
-        -
-        <a href='#'>
+        </NavLink>
+        <NavLink to={`./articles/data/nazwa#save`}>
           <i className='far fa-bookmark'></i> Zapisz
-        </a>
-        -
-        <a href='#'>
+        </NavLink>
+        <NavLink to={`./user/report`}>
           <i className='far fa-flag'></i> Zgłoś
-        </a>
+        </NavLink>
       </div>
     </div>
   );
