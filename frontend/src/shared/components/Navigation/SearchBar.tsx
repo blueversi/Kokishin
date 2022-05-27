e6a6ca0d37f974bb57f80462b2bdb9ca2af98ca4
Rafał <riderbrand@gmail.com>
2022-05-27 21:43:20 +0200

    Add state and handlers for search bar

--- frontend/src/shared/components/Navigation/SearchBar.tsx
+++ frontend/src/shared/components/Navigation/SearchBar.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSearchInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchInput(event.target.value);
+  };
+
+  const handleSearchButtonClick = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(
+      'tu bedzie logika wyszukiwania w przyszlości a narazie czyszcze stan'
+    );
+    setSearchInput('');
+  };
+
   return (
     <div className='search-bar'>
-      <form action=''>
+      <form onSubmit={handleSearchButtonClick}>
         <input
           id='searchBar'
           type='text'
           name='searchBar'
           placeholder='Wyszukaj'
+          value={searchInput}
+          onChange={handleSearchInputChange}
         />
         <button type='submit' className='btn btn-default' title='search'>
           <i className='fas fa-search'></i>
