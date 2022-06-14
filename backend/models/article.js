dc03f46ea0439887aea09ef7ee4ed9651e1561a3
Rafał <riderbrand@gmail.com>
2022-06-14 23:22:06 +0200

    feat: add mongoose article schema

---
+++ backend/models/article.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  author: { type: Number, required: true },
+  date: { type: String, required: true },
+  img: { type: String, required: true },
+  author: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Article', articleSchema);
