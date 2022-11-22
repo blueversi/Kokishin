bac056031284f5982699a5fc86b9f4fef7a75775
Rafał <riderbrand@gmail.com>
2022-11-22 22:39:42 +0100

    refactor(fron): Remove unecessary  literal concat

--- frontend/src/APIcalls/ArticlesCalls.tsx
+++ frontend/src/APIcalls/ArticlesCalls.tsx
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const getAllArticles = () => {
   return axios.get('http://localhost:5000/articles/').catch(function (error) {
-    console.log('GET ERROR' + '\n' + error);
+    console.log('GET ERROR\n' + error);
   });
 };
--- frontend/src/APIcalls/SidebarCalls.tsx
+++ frontend/src/APIcalls/SidebarCalls.tsx
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const getSidebarItems = () => {
   return axios.get('http://localhost:5000/sidebar/').catch(function (error) {
-    console.log('GET ERROR' + '\n' + error);
+    console.log('GET ERROR\n' + error);
   });
 };
