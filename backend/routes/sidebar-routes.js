eab095fb2e00d700cbf4fbcc8574aec0421c67c0
Rafał <riderbrand@gmail.com>
2023-01-19 22:21:17 +0100

    refactor(backend): minor changes

--- backend/routes/sidebar-routes.js
+++ backend/routes/sidebar-routes.js
@@ -1,7 +1,7 @@
-var express = require('express');
+const express = require('express');
 const { check } = require('express-validator');
 const sidebarController = require('../controllers/sidebar-controlers');
-var router = express.Router();
+const router = express.Router();
 
 /* GET All sidebar items. */
 router.get('/', sidebarController.getAllSidebarItems);
