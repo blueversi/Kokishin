637fe783253f9e486f60ba94c639a182adddf0c3
Rafał <riderbrand@gmail.com>
2023-03-29 23:37:42 +0200

    refactor(frontend): routing changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import Footer from './shared/components/Foooter/Footer';
 import HomePage from './places/pages/HomePage';
 import Profile from './user/pages/Profile';
 import Page404 from './places/pages/page404';
+import Login from './places/pages/Login';
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
           <Route path='/user/BlueVersi' element={<Profile />} />
           <Route path='/404' element={<Page404 />} />
           <Route path='*' element={<Navigate to='/404' replace />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
         <Footer />
       </Router>
